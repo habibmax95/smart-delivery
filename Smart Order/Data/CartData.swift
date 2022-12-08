97bb0ee0b7d4446ff5bca1239086ba9d064085c3
Habibur Rahman <hr_habib@hotmail.com>
2022-12-08 11:52:10 +0100

    Updated sample json and cart data

--- Smart Order/Data/CartData.swift
+++ Smart Order/Data/CartData.swift
@@ -8,10 +8,10 @@
 import Foundation
 
 let cartsData = [
-    Product(productId: 1, name: "Apple", deliveryDays: [Weekday.sunday, Weekday.monday, Weekday.wednesday], productType: ProductType.normal, daysInAdvance: 3),
-    Product(productId: 2, name: "Spenaut", deliveryDays: [Weekday.sunday, Weekday.tuesday, Weekday.monday], productType: ProductType.temporary, daysInAdvance: 6),
-    Product(productId: 3, name: "Chinese Okra", deliveryDays: [Weekday.sunday, Weekday.monday, Weekday.wednesday], productType: ProductType.external, daysInAdvance: 4),
-    Product(productId: 4, name: "Shampoo", deliveryDays: [Weekday.sunday, Weekday.monday, Weekday.tuesday, Weekday.wednesday], productType: ProductType.normal, daysInAdvance: 1),
-    Product(productId: 5, name: "Diska", deliveryDays: [Weekday.sunday, Weekday.monday, Weekday.wednesday], productType: ProductType.normal, daysInAdvance: 8),
+    Product(productId: 1, name: "Apple", deliveryDays: [Weekday.saturday, Weekday.sunday, Weekday.monday, Weekday.wednesday], productType: ProductType.normal, daysInAdvance: 1),
+    Product(productId: 2, name: "Spenaut", deliveryDays: [Weekday.saturday, Weekday.sunday, Weekday.tuesday, Weekday.monday, Weekday.friday], productType: ProductType.temporary, daysInAdvance: 1),
+//    Product(productId: 3, name: "Chinese Okra", deliveryDays: [Weekday.sunday, Weekday.monday, Weekday.wednesday], productType: ProductType.external, daysInAdvance: 4),
+    Product(productId: 4, name: "Shampoo", deliveryDays: [Weekday.saturday, Weekday.sunday, Weekday.monday, Weekday.tuesday, Weekday.wednesday], productType: ProductType.normal, daysInAdvance: 1),
+    Product(productId: 5, name: "Diska", deliveryDays: [Weekday.saturday, Weekday.sunday, Weekday.monday, Weekday.wednesday], productType: ProductType.normal, daysInAdvance: 1),
     
 ]
